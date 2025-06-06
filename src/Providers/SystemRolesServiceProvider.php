eb01a0a9c4dfff4a6542c8c892458ccc7a921f80
Solomon Bareebe <baberuka@gmail.com>
2025-06-06 21:51:04 +0200

    Update SystemRolesServiceProvider.php

--- src/Providers/SystemRolesServiceProvider.php
+++ src/Providers/SystemRolesServiceProvider.php
@@ -64,7 +64,7 @@ class SystemRolesServiceProvider extends ServiceProvider
             __DIR__.'/../../assets/fonts' => public_path('vendor/systemroles/fonts'),  
             __DIR__.'/../../assets/images' => public_path('vendor/systemroles/images'),  
             __DIR__.'/../../assets/favicon' => public_path('vendor/systemroles/favicon'),  
-            _DIR__.'/../../assets/addons' => public_path('vendor/systemroles/addons'),  
+            __DIR__.'/../../assets/addons' => public_path('vendor/systemroles/addons'),  
         ], 'systemroles-assets');
 
         $this->publishes([
